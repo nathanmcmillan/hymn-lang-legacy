6a05050d1788a9f6f0f90a61557054f3c30cc7eb
nmm <nathannoob@gmail.com>
2019-08-03 23:56:03 -0400

    readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # hymn-lang
 compiles to readable C code
+
+```
+type foo<T>
+  data T
+
+main
+  f = foo<string>
+    data: "hello world"
+  echo(f.data)
+```
+
+### features
+* generics
+* goto
+* labels
+* enums
+* unions
+* structs
+* matching
+
+### todo
+* slices
+* scope
+* stack variables
+* references to primitives
+* free heap space
+* interfaces
+* dictionaries
--- go/calc.go
+++ go/calc.go
@@ -1,10 +1,7 @@
 package main
 
-import "fmt"
-
 func (me *parser) prefix() *node {
 	op := me.token.is
-	fmt.Println("prefix", op)
 	if pre, ok := prefixes[op]; ok {
 		return pre.fn(me, op)
 	}
@@ -13,7 +10,6 @@ func (me *parser) prefix() *node {
 
 func (me *parser) infix(left *node) *node {
 	op := me.infixOp()
-	fmt.Println("infix", op)
 	if inf, ok := infixes[op]; ok {
 		return inf.fn(me, left, op)
 	}
@@ -21,17 +17,13 @@ func (me *parser) infix(left *node) *node {
 }
 
 func (me *parser) calc(precedence int) *node {
-	fmt.Println("calc", precedence)
 	node := me.prefix()
 	for {
 		op := me.infixOp()
-		fmt.Println("precedence :=", precedence)
-		fmt.Println(op, "infix precedence :=", getInfixPrecedence(op))
 		if precedence >= getInfixPrecedence(op) {
 			break
 		}
 		node = me.infix(node)
 	}
-	fmt.Println("calc return", node.string(0))
 	return node
 }

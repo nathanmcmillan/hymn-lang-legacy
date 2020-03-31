07545f6a8d79382efbf0f0e917196ce618ef4b1c
nmm-mac <gameinbucket@github.com>
2020-03-31 19:08:51 -0400

    better match single line support

--- go/parse-match.go
+++ go/parse-match.go
@@ -1,149 +1,5 @@
 package main
 
-func (me *parser) parseIs(left *node, op string, n *node) (*node, *parseError) {
-	n.setData(newdataprimitive("bool"))
-	if er := me.eat(op); er != nil {
-		return nil, er
-	}
-
-	negate := false
-	if me.token.is == "not" {
-		if er := me.eat("not"); er != nil {
-			return nil, er
-		}
-		negate = true
-	}
-
-	var right *node
-	data := left.data()
-	if data.isSomeOrNone() {
-		is := ""
-		if me.token.is == "some" {
-			if er := me.eat("some"); er != nil {
-				return nil, er
-			}
-			if negate {
-				is = "none"
-			} else {
-				is = "some"
-			}
-		} else if me.token.is == "none" {
-			if er := me.eat("none"); er != nil {
-				return nil, er
-			}
-			if negate {
-				is = "some"
-			} else {
-				is = "none"
-			}
-		} else {
-			return nil, err(me, ECodeBadIsStatement, "right side of \"is\" was \""+me.token.is+"\"")
-		}
-		if is == "some" {
-			right = nodeInit("some")
-			if me.token.is == "(" {
-				if negate {
-					return nil, err(me, ECodeNegationProhibited, "Negation not allowed when declaring a variable here.")
-				}
-				if er := me.eat("("); er != nil {
-					return nil, er
-				}
-				temp := me.token.value
-				if er := me.eat("id"); er != nil {
-					return nil, er
-				}
-				if er := me.eat(")"); er != nil {
-					return nil, er
-				}
-				tempd := data.getmember().getnamedvariable(temp, false)
-				tempv := nodeInit("variable")
-				tempv.idata = newidvariable(me.hmfile, tempd.name)
-				tempv.copyData(tempd.data())
-				tempv.push(left)
-				varnode := &variableNode{tempv, tempd}
-				me.hmfile.enumIsStack = append(me.hmfile.enumIsStack, varnode)
-
-				// TODO :: cleanup the above enumIsStack
-				tempvv := nodeInit("variable")
-				tempvv.idata = newidvariable(me.hmfile, tempd.name)
-				tempvv.copyData(tempd.data())
-				right.push(tempvv)
-				//
-			}
-		} else if is == "none" {
-			right = nodeInit("none")
-			if me.token.is == "(" {
-				if negate {
-					return nil, err(me, ECodeNegationProhibited, "Negation not allowed when declaring a variable here.")
-				}
-				return nil, err(me, ECodeNoneTypeValueProhibited, "none type can't have a value here.")
-			}
-		}
-	} else {
-		baseEnum, _, ok := data.isEnum()
-		if !ok {
-			return nil, err(me, ECodeIsStatementExpectedEnum, "Left side of \"is\" must be enum but was: "+data.error())
-		}
-		if me.token.is == "id" {
-			name := me.token.value
-			if un := baseEnum.getType(name); un != nil {
-				if er := me.eat("id"); er != nil {
-					return nil, er
-				}
-				newenum := newdataenum(me.hmfile, baseEnum, un, copydatalist(data.generics))
-				if negate {
-					right = nodeInit("negate-match-enum")
-				} else {
-					right = nodeInit("match-enum")
-				}
-				right.setData(newenum)
-				if me.token.is == "(" {
-					if negate {
-						return nil, err(me, ECodeNegationProhibited, "Negation not allowed when declaring a variable here.")
-					}
-					if er := me.eat("("); er != nil {
-						return nil, er
-					}
-					temp := me.token.value
-					if er := me.eat("id"); er != nil {
-						return nil, er
-					}
-					if er := me.eat(")"); er != nil {
-						return nil, er
-					}
-					tempd := right.data().getnamedvariable(temp, false)
-					tempv := nodeInit("variable")
-					tempv.idata = newidvariable(me.hmfile, tempd.name)
-					tempv.copyData(tempd.data())
-					tempv.push(left)
-					varnode := &variableNode{tempv, tempd}
-					me.hmfile.enumIsStack = append(me.hmfile.enumIsStack, varnode)
-
-					// TODO :: cleanup the above enumIsStack
-					tempvv := nodeInit("variable")
-					tempvv.idata = newidvariable(me.hmfile, tempd.name)
-					tempvv.copyData(tempd.data())
-					right.push(tempvv)
-					//
-				}
-			} else {
-				var er *parseError
-				right, er = me.calc(getInfixPrecedence(op), nil)
-				if er != nil {
-					return nil, er
-				}
-			}
-		} else if checkIsPrimitive(me.token.is) {
-			return nil, err(me, ECodeCannotMatchOnPrimitive, "can't match on a primitive. did you mean to use an enum implementation?")
-		} else {
-			return nil, err(me, ECodeUnknownType, "Unknown right side of is: "+me.token.is)
-		}
-	}
-	n.push(left)
-	n.push(right)
-	return n, nil
-}
-
 func (me *parser) parseMatch() (*node, *parseError) {
 	depth := me.token.depth
 	if er := me.eat("match"); er != nil {
@@ -176,14 +32,28 @@ func (me *parser) parseMatch() (*node, *parseError) {
 
 	n.push(matching)
 
-	if er := me.newLine(); er != nil {
-		return nil, er
+	single := true
+	multi := false
+
+	if me.isNewLine() {
+		single = false
+		me.newLine()
+	} else if me.token.is == ":" {
+		me.next()
+	} else {
+		return nil, err(me, ECodeUnexpectedToken, "Expected line or `:`")
 	}
 
 	for {
-		if me.token.depth <= depth {
+		if single {
+			if multi {
+				break
+			}
+		} else if me.token.depth <= depth {
 			break
-		} else if me.token.is == "id" {
+		}
+		multi = true
+		if me.token.is == "id" {
 			name := me.token.value
 			if er := me.eat("id"); er != nil {
 				return nil, er
@@ -226,12 +96,15 @@ func (me *parser) parseMatch() (*node, *parseError) {
 					return nil, er
 				}
 				n.push(b)
-			} else {
+			} else if me.token.is == ":" {
+				me.next()
 				e, er := me.expression()
 				if er != nil {
 					return nil, er
 				}
 				n.push(e)
+			} else {
+				return nil, err(me, ECodeUnexpectedToken, "Expected line or `:`")
 			}
 			if me.isNewLine() {
 				me.newLine()
@@ -278,12 +151,15 @@ func (me *parser) parseMatch() (*node, *parseError) {
 					return nil, er
 				}
 				n.push(b)
-			} else {
+			} else if me.token.is == ":" {
+				me.next()
 				e, er := me.expression()
 				if er != nil {
 					return nil, er
 				}
 				n.push(e)
+			} else {
+				return nil, err(me, ECodeUnexpectedToken, "Expected line or `:`")
 			}
 			if me.isNewLine() {
 				me.newLine()
@@ -303,12 +179,15 @@ func (me *parser) parseMatch() (*node, *parseError) {
 					return nil, er
 				}
 				n.push(b)
-			} else {
+			} else if me.token.is == ":" {
+				me.next()
 				e, er := me.expression()
 				if er != nil {
 					return nil, er
 				}
 				n.push(e)
+			} else {
+				return nil, err(me, ECodeUnexpectedToken, "Expected line or `:`")
 			}
 			if me.isNewLine() {
 				me.newLine()
@@ -325,12 +204,15 @@ func (me *parser) parseMatch() (*node, *parseError) {
 					return nil, er
 				}
 				n.push(b)
-			} else {
+			} else if me.token.is == ":" {
+				me.next()
 				e, er := me.expression()
 				if er != nil {
 					return nil, er
 				}
 				n.push(e)
+			} else {
+				return nil, err(me, ECodeUnexpectedToken, "Expected line or `:`")
 			}
 			if me.isNewLine() {
 				me.newLine()
@@ -373,12 +255,15 @@ func (me *parser) parseMatch() (*node, *parseError) {
 					return nil, er
 				}
 				n.push(b)
-			} else {
+			} else if me.token.is == ":" {
+				me.next()
 				e, er := me.expression()
 				if er != nil {
 					return nil, er
 				}
 				n.push(e)
+			} else {
+				return nil, err(me, ECodeUnexpectedToken, "Expected line or `:`")
 			}
 			if me.isNewLine() {
 				me.newLine()
